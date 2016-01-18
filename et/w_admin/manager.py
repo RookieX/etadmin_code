593c235263a5685340fab630d519b7760263b269
RookieX <xpc-lx@hotmail.com>
2016-01-18 18:06:55 +0800

    添加tornado，运行web程序无误

--- et/w_admin/manager.py
+++ et/w_admin/manager.py
@@ -1,3 +1,47 @@
 # -*- coding: utf-8 -*-
 # Date: 16-1-18
 # Author: 徐鹏程
+
+
+import os
+import sys
+
+import config
+
+from et.common.helpers import sys_path_extender
+
+sys_path_extender.extend(config.third_party_path)
+
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+
+
+class WebApp(tornado.web.Application):
+    def __init__(self):
+        super(self.__class__, self).__init__(**mk_settings())
+
+
+class PingHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('login.html')
+
+
+def mk_settings():
+    return {
+        'handlers': [(r'/ping', PingHandler)],
+        'static_path': os.path.join(os.path.dirname(__file__), 'static'),
+        'template_path': os.path.join(os.path.dirname(__file__), 'templates')
+    }
+
+
+def main():
+    port = sys.argv[1]
+
+    http_server = tornado.httpserver.HTTPServer(WebApp(), xheaders=True)
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
