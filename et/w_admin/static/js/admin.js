a9c65c163fc339636777b1a1d960fbf801769da6
RookieX <xpc-lx@hotmail.com>
2016-02-01 12:15:44 +0800

    优化dropdownlist

--- et/w_admin/static/js/admin.js
+++ et/w_admin/static/js/admin.js
@@ -16,16 +16,60 @@
 })(window.jQuery);
 
 $(function () {
+
+    //显示下拉框
+    function showDropdownList(ddl) {
+        ddl.children('ul').slideDown('fast');
+    }
+
+    //隐藏下拉框
+    function hideDropdownList(ddl) {
+        ddl.children('ul').slideUp('fast');
+    }
+
+    //显示/隐藏下拉框
+    function tolggleDropdownList(ddl) {
+        ddl.children('ul').slideToggle('fast');
+    }
+
+    //设置下拉框的选中值
+    function setDropdownListValue(ddl, li) {
+        var btn = ddl.children('.dropdown-btn');
+        var txt = btn.find('.dropdown-text');
+
+        btn.val(li.val());
+        txt.text(li.text());
+
+        li.attr('item-selected','true').siblings().removeAttr('item-selected');
+    }
+
+    $('.dropdown').each(function (index, item) {
+        var self = $(item);
+        var selected = self.find('ul li[item-selected]');
+        if (selected.length > 0) {
+            setDropdownListValue(self, selected);
+        }
+    });
+
+    $('.dropdown .dropdown-btn').click(function (e) {
+        var self = $(this);
+        tolggleDropdownList($(this).parent());
+        e.stopPropagation();
+    });
+
+    $('.dropdown ul li').click(function () {
+        var self = $(this);
+
+        var dropdown = self.parent().parent();
+
+        setDropdownListValue(dropdown, self);
+
+        hideDropdownList(dropdown);
+    });
+
     $(document).click(function () {
-        $('.dropdown ul').slideUp('fast');
+        hideDropdownList($('.dropdown'));
     });
 
     $('.tablelist thead :checkbox').chkCheckAll($('.tablelist tbody :checkbox'));
-
-    $('.dropdown ul').hide();
-    $('.dropdown .btn').click(function (e) {
-        var self = $(this);
-        $(this).parent().children('ul').slideToggle('fast');
-        e.stopPropagation();
-    });
 });
